ba7efe8344c85e3cda14b89192693b362775b3a0
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-04-18 16:46:38 +0700

    Create datamahasiswa.py

---
+++ Coding/Source Code/datamahasiswa.py
@@ -0,0 +1,9 @@
+nama = input("Masukan nama anda : ")
+npm = int(input("Masukan npm anda : "))
+prodi = input("Masukan prodi anda : ")
+
+print("----------------------------------------------------")
+print("Nama : adalah", nama)
+print("NPM : adalah", npm)
+print("Prodi : adalah", prodi)
+print("----------------------------------------------------")
