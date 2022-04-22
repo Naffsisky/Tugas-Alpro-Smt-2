9d9fe8ba9f8bc5075caa296d54f04fe8011e2433
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-04-22 14:46:16 +0700

    Create angkabelakangkoma.cpp

---
+++ Coding/Source Code/angkabelakangkoma.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+float pi;
+
+int main() {   
+    pi = 3.14159265358979323846;
+    cout << setprecision(3) << "pi = " << pi << endl;
+}
