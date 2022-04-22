f58bf6bc3c3535f7b40e6c944a6cddc10d19f735
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-04-22 13:57:51 +0700

    Create whileloop.cpp

---
+++ Coding/Source Code/whileloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int i;
+
+int main() {
+    i = 1;
+    while (i <= 5){
+    cout << i << endl;
+    i++;
+  }
+  cout << "Total bilangan berjumlah : " << (i - 1);
+  return 0;
+}
