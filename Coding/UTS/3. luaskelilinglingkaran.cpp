e0e9d4984f8fd8685433d6577406cd0e2b4b34bc
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-04-22 10:24:22 +0700

    Create 3. luaskelilinglingkaran.cpp

---
+++ Coding/UTS/3. luaskelilinglingkaran.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int r,keliling, luas;
+
+int main(){
+
+cout << " Masukan Jari-Jari Lingkaran : "; 
+cin >> r;
+
+keliling = 2*3.14*r;
+cout << " Keliling : " << keliling << endl;
+
+luas=3.14*r*r;
+cout << " Luas : " << luas;
+
+return 0;
+}
