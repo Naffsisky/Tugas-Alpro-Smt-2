cc3c45e0bf82594cc542b460d72fa8985327a961
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-04-29 15:26:33 +0700

    Membuat bintang dengan for loop

---
+++ Coding/Source Code/bintang.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int user, i, j;
+
+int main(){
+	
+    cout<<"Masukkan bilangan : ";
+    cin>>user;
+
+    if(user){
+        for(i = user; i >= 1; i--){
+            cout<<"\n";
+        for(j = 1; j <= i; j++){
+            cout<<"*"<<" ";
+            }
+        }
+    }
+    else{
+    cout<<"Bilangan tidak sesuai!";
+    }
+    return 0;
+}
